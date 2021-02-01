f2ffbd95bda3eec20eeb1b7b9b94ed31706c7e7a
mpadge <mark.padgham@email.com>
2021-02-01 13:42:02 +0100

    fix tests with new cli output

--- tests/testthat/test-test-dummy-pkg.R
+++ tests/testthat/test-test-dummy-pkg.R
@@ -11,11 +11,17 @@ test_that("dummy package", {
 
               x <- capture.output (roxygen2::roxygenise (d), type = "message")
 
-              expect_true (any (grepl ("R files:", x)))
+              expect_true (any (grepl ("R files", x)))
               expect_true (any (grepl ("Standards \\[G1\\.1\\]", x)))
-              expect_true (any (grepl ("tests files:", x)))
 
-              # TODO: Fix the next two:
-              expect_false (any (grepl ("Standards \\[G2\\.2\\]", x)))
-              expect_true (any (grepl ("Standards \\[G2\\.2", x)))
+              expect_true (any (grepl ("tests files", x)))
+              expect_true (any (grepl ("Standards \\[G2\\.2\\]", x)))
+              # G2.2 is in "tests files":
+              expect_true (grep ("Standards \\[G2\\.2\\]", x) [1] >
+                           grep ("tests files", x) [1])
+
+              expect_true (any (grepl ("src files", x)))
+              expect_true (any (grepl ("Standards \\[G3\\.3\\]", x)))
+              expect_true (grep ("Standards \\[G3\\.3\\]", x) [1] >
+                           grep ("src files", x) [1])
 })
