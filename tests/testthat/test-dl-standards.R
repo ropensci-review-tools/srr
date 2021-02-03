51ee005d9444b3b9d8cf3639a87632792a9a4a27
mpadge <mark.padgham@email.com>
2021-02-03 12:34:48 +0100

    include test of checklist_check

--- tests/testthat/test-dl-standards.R
+++ tests/testthat/test-dl-standards.R
@@ -1,3 +1,5 @@
+
+
 test_that("download standards", {
 
               Sys.setenv("CLIPR_ALLOW" = TRUE)
@@ -13,7 +15,8 @@ test_that("download standards", {
               expect_type (s, "character")
               expect_true (length (s) > 100)
               expect_true (grepl ("General Standards", s [1]))
-              expect_true (any (grepl ("Regression and Supervised Learning Standards", s)))
+              txt <- "Regression and Supervised Learning Standards"
+              expect_true (any (grepl (txt, s)))
               expect_true (any (grepl ("Machine Learning Standards", s)))
 
               expect_identical (s, readLines (filename))
@@ -23,5 +26,20 @@ test_that("download standards", {
                   type = "message"
               )
               expect_identical (s, s2)
-              expect_true (any (grepl ("No formatting issues found in file", x)))
+              txt <- "No formatting issues found in file"
+              expect_true (any (grepl (txt, x)))
+
+              # ------- test checklist_check: -------
+              i <- grep ("\\*\\*G", s2) [1]
+              # change one bold markdown format to single "*":
+              s2 [i] <- gsub ("\\*\\*G", "\\*G", s2 [i])
+              writeLines (s2, filename)
+              x <- capture.output (
+                  s3 <- rssr_checklist_check (filename),
+                  type = "message"
+              )
+              txt <- "file contained incorrect formatting and has been modified"
+              expect_true (any (grep (txt, x)))
+              expect_true (s3 [i] != s2 [i]) # file has been fixed
+              expect_true (grepl ("\\*\\*G", s3 [i]))
 })
