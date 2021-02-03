6af7a8a4ca8336dd52b53904c712d38c22f07c27
mpadge <mark.padgham@email.com>
2021-02-03 11:48:18 +0100

    add test-categories

---
+++ tests/testthat/test-categories.R
@@ -0,0 +1,16 @@
+test_that("available categories", {
+
+              expect_silent (
+                  x <- rssr_available_categories ()
+                  )
+              expect_s3_class (x, "data.frame")
+              expect_equal (ncol (x), 3)
+              expect_identical (names (x),
+                                c ("category", "title", "url"))
+              expect_true (nrow (x) >= 8)
+              expect_true (all (c ("bayesian", "eda", "general", "ml",
+                                   "regression", "spatial", "time-series",
+                                   "unsupervised") %in%
+                            x$category))
+
+})
