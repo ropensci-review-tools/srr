55d9d81c0d55203951aadc7b91c19245f4312304
mpadge <mark.padgham@email.com>
2021-02-04 14:02:59 +0100

    rm commented-out line

--- R/roclet.R
+++ R/roclet.R
@@ -122,7 +122,6 @@ collect_one_tag <- function (base_path, blocks, test_blocks, rcpp_blocks,
 process_rssr_tags <- function (block, fn_name = TRUE, dir = "R") {
 
     func_name <- block$object$alias
-    #standards <- roxygen2::block_get_tag_value (block, "rssr")
     standards <- roxygen2::block_get_tags (block, "rssr")
     standards <- unlist (lapply (standards, function (i) i$val))
     # function to ensure first of any multi line standards ends with comma, and
