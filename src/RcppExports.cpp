b6aea3f37a0e24c10b46976cdf4cfaefff377f6a
mpadge <mark.padgham@email.com>
2021-11-22 12:02:20 +0100

    update RcppExports

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // rcpp_parse_rmd
 void rcpp_parse_rmd(const std::string filename, const std::string tempfile);
 RcppExport SEXP _srr_rcpp_parse_rmd(SEXP filenameSEXP, SEXP tempfileSEXP) {
