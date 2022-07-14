be565ea195b4eef3f00dd0b07c6601bf67c5a652
mpadge <mark.padgham@email.com>
2022-07-14 14:19:22 +0200

    spaceout R/roclet-checks.R

--- R/roclet-checks.R
+++ R/roclet-checks.R
@@ -3,9 +3,9 @@
 
 msgs_to_standards <- function (msgs) {
     res <- lapply (msgs, function (i) {
-                       re <- regexpr ("(?=\\[).*?(?<=\\])", i, perl = T)
-                       res <- gsub ("^\\[|\\]$", "", regmatches (i, re))
-                       strsplit (res, ",\\s") [[1]]    })
+        re <- regexpr ("(?=\\[).*?(?<=\\])", i, perl = T)
+        res <- gsub ("^\\[|\\]$", "", regmatches (i, re))
+        strsplit (res, ",\\s") [[1]]    })
     return (unique (unlist (res)))
 }
 
@@ -13,20 +13,24 @@ mixed_tag_error <- function (stds1, stds2, tag1, tag2) {
 
     index <- which (stds1 %in% stds2)
     if (length (index) > 0) {
-        stop ("Standards [", paste0 (stds1 [index], collapse = ", "),
-              "] are listed with both ", tag1, " and ", tag2, " tags.\n",
-              "Please rectify to ensure these standards are only ",
-              "associated with one tag.",
-        call. = FALSE)
+        stop (
+            "Standards [", paste0 (stds1 [index], collapse = ", "),
+            "] are listed with both ", tag1, " and ", tag2, " tags.\n",
+            "Please rectify to ensure these standards are only ",
+            "associated with one tag.",
+            call. = FALSE
+        )
     }
 
     index <- which (stds2 %in% stds1)
     if (length (index) > 0) {
-        stop ("Standards [", paste0 (stds2 [index], collapse = ", "),
-              "] are listed with both ", tag1, " and ", tag2, " tags.\n",
-              "Please rectify to ensure these standards are only ",
-              "associated with one tag.",
-        call. = FALSE)
+        stop (
+            "Standards [", paste0 (stds2 [index], collapse = ", "),
+            "] are listed with both ", tag1, " and ", tag2, " tags.\n",
+            "Please rectify to ensure these standards are only ",
+            "associated with one tag.",
+            call. = FALSE
+        )
     }
 
     return (TRUE)
