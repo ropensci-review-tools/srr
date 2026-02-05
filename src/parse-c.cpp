44f9abae458f7b0f1eddfbb544dbbcf98eebe564
mpadge <mark.padgham@email.com>
2026-02-05 10:05:34 +0100

    update parse-c.cpp to match rust structure for #54
    Replace all non-srr lines with empty comment lines

--- src/parse-c.cpp
+++ src/parse-c.cpp
@@ -25,6 +25,7 @@ void rcpp_parse_c (const std::string filename,
     in_file.seekg (0); 
 
     bool in_cmts = false;
+    bool in_srr = false;
 
     while (getline (in_file, linetxt, '\n')) { 
         
@@ -32,7 +33,22 @@ void rcpp_parse_c (const std::string filename,
 
         if (c::is_c_cmt (linetxt)) {
             in_cmts = true;
-            c::replace_c_cmt (linetxt);
+            bool start_srr = c::is_c_cmt_with_srr (linetxt);
+            bool line_is_empty = c::is_empty_c_cmt (linetxt);
+            if (start_srr) {
+                in_srr = true;
+            }
+            if (in_srr) {
+                if (!line_is_empty) {
+                    c::replace_c_cmt (linetxt);
+                } else {
+                    in_srr = false;
+                    linetxt = "#'";
+                }
+            } else {
+                linetxt = "#'";
+            }
+
             out_file << linetxt << std::endl;
         } else {
             if (in_cmts) {
@@ -50,9 +66,28 @@ void rcpp_parse_c (const std::string filename,
 
 bool c::is_c_cmt (const std::string &line)
 {
-    const std::string c_cmt_def = "//'";
+    const std::string ptn = "//'";
 
-    return (line.compare (0, 3, c_cmt_def) == 0);
+    return (line.compare (0, 3, ptn) == 0);
+}
+
+bool c::is_empty_c_cmt (const std::string &line)
+{
+    const std::string ptn = "//'";
+
+    if (line.compare (0, 3, ptn) != 0) {
+        return false;
+    }
+
+    return std::all_of(line.begin() + 3, line.end(),
+        [](unsigned char c) { return std::isspace(c); });
+}
+
+bool c::is_c_cmt_with_srr (const std::string &line)
+{
+    const std::string ptn = "//' @srrstats";
+
+    return (line.compare (0, 13, ptn) == 0);
 }
 
 void c::replace_c_cmt (std::string &line)
